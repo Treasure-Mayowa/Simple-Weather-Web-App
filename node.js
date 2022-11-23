5e55127c76e4961571068aa4ba340abb83eb6ad4
Treasure-Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2022-11-23 03:51:32 -1200

    Ctrl + ENTER

--- node.js
+++ node.js
@@ -1,54 +1,39 @@
-const apiKey = "dbab25302d9476cb4e7dfbdd4f23cff2";
+//Javascript calling the API
+const API_KEY = `dbab25302d9476cb4e7dfbdd4f23cff2`
+const form = document.querySelector("form")
+const search = document.querySelector(".search")
+const weather = document.querySelector("#weather")
+    // const API = `https://api.openweathermap.org/data/2.5/weather?
+    // q=${city}&appid=${API_KEY}&units=metric`
+    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-  
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
+    }
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
 
-
-async function getWeatherByLocation(city){
-     
-         const resp = await fetch(url(city), {
-             origin: "cros" });
-         const respData = await resp.json();
-     
-           addWeatherToPage(respData);
-          
-     }
-
-      function addWeatherToPage(data){
-          const temp = Ktoc(data.main.temp);
-
-          const weather = document.createElement('div')
-          weather.classList.add('weather');
-
-          weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-          <small>${data.weather[0].main}</small>
-          
-          `;
-
-
-        //   cleanup 
-          main.innerHTML= "";
-           main.appendChild(weather);
-      };
-
-
-     function Ktoc(K){
-         return Math.floor(K - 273.15);
-     }
-
-
-
-     form.addEventListener('submit',(e) =>{
-        e.preventDefault();
-
-        const city = search.value;
-
-        if(city){
-            getWeatherByLocation(city)
-        }
-
-     });
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
