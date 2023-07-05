3f2e50e2ba3bd562663eb645e482832230835bf6
Treasure-Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-07-06 00:30:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//Javascript calling the API
+// Storing the values in variables
 const API_KEY = `dbab25302d9476cb4e7dfbdd4f23cff2`
 const form = document.querySelector("form")
 const search = document.querySelector(".search")
 const weather = document.querySelector("#weather")
-    // const API = `https://api.openweathermap.org/data/2.5/weather?
-    // q=${city}&appid=${API_KEY}&units=metric`
-    // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
 const getWeather = async(city) => {
     weather.innerHTML = `<h2> Loading... <h2>`
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
